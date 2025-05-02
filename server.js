65528055ef697f1a9ee837ca559f671340c71b18
hwuiinn <82766211+Huwiinn@users.noreply.github.com>
2025-05-02 13:47:41 +0900

    set: html, js, css, local server

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'application/javascript',
+    '.css': 'text/css',
+}
+
+const root = process.cwd();
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(root, req.url === '/' ? 'index.html' : req.url);
+
+    try {
+        const stat = fs.statSync(filePath);
+
+        console.log('stat : ', stat);
+        console.log('filePath : ', filePath);
+
+        if(stat.isDirectory()) {
+            filePath = path.join(filePath, 'index.html');
+        }
+
+        const ext = path.extname(filePath);
+        const contentType = mimeTypes[ext] || 'application/octet-stream';
+        res.writeHead(200, {'Content-Type': contentType});
+
+        fs.createReadStream(filePath).pipe(res);
+    } catch(error) {
+        console.log(`error : ${error.message}`);
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 Not Found');
+    }
+})
+
+server.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
+
