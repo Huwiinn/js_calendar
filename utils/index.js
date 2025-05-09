b7896f56868c3e1668d8d6641e8bc2d7e01abe22
hwuiinn <gnldls8406@gmail.com>
2025-05-09 23:09:43 +0900

    set: function - 지난달, 이번달, 다음달 요일 카운팅 함수 세팅 - 기능 구현전

--- utils/index.js
+++ utils/index.js
@@ -9,3 +9,12 @@ export const getSunday = (year, month, day) => {
 
   return null;
 };
+
+// 이번달 대비 지난달 요일 노출 카운팅 함수
+export const getPrevMonthDay = () => {};
+
+// 이번달 요일 카운팅 함수
+export const getCurrentMonthDay = () => {};
+
+// 이번달 기준 다음달 요일 노출 카운팅 함수
+export const getNextMonthDay = () => {};
